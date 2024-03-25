f9e9739a28e7e5a1009e4b4e1bed9b1e802711db
Enrique Leal Isla <leleal@cecapmex.com>
2024-03-25 12:32:02 -0600

    Ajuste servicios

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,85 +12,53 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             #region Variable
-            int[] arr = new int[100];
-            long[] arrLong = new long[100];
-            List<int> ar = arr.ToList();
-            List<int> a = arr.ToList();
-
-            List<int> b = arr.ToList();
-
-            int arCount = arr.Count();
-
-            List<long> ar_Long = arrLong.ToList();
-
-            int result_int = 0;
-
-            // Generar 100 números positivos aleatorios
-            Random random = new Random();
-            int Ejercicio = 0;
-            int val1 = 0;
-            int val2 = 0;
-            int sum = 0;
-            #endregion
+            int[] arr = new int[100]; // Crear un array de 100 elementos
             for (int i = 0; i < arr.Length; i++)
             {
-                arr[i] = random.Next(1, 1001);
+                arr[i] = i + 1; // Llenar el array con valores del 1 al 100
             }
-            Console.WriteLine("Acceso");
 
-            switch (Ejercicio)
+            long[] arrLong = new long[100]; // Crear un array de long de 100 elementos
+            for (int i = 0; i < arrLong.Length; i++)
+            {
+                arrLong[i] = i + 1000; // Llenar el array con valores del 1000 al 1099
+            }
+
+            List<int> ar = arr.ToList(); // Convertir el array 'arr' en una lista
+            List<int> a = arr.ToList(); // Convertir el array 'arr' en otra lista
+
+            List<int> b = arr.ToList(); // Convertir el array 'arr' en otra lista
+
+            int arCount = arr.Length; // Obtener la longitud del array 'arr'
+
+            List<long> ar_Long = arrLong.ToList(); // Convertir el array 'arrLong' en una lista de long
+
+            int result_int = 42; // Asignar un valor de ejemplo a 'result_int'
+
+            int Ejercicio = 1; // Definir un valor de ejercicio (por ejemplo, 1)
+
+            int val1 = 10; // Asignar un valor de ejemplo a 'val1'
+            int val2 = 20; // Asignar un valor de ejemplo a 'val2'
+            int sum = val1 + val2; // Calcular la suma de 'val1' y 'val2'
+
+            #endregion
+
+
+
+            RandomService randomService = new RandomService();
+
+            Console.WriteLine("Elige una opción:");
+            // Agrega más opciones según sea necesario...
+
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
             {
                 case 1:
-                    #region Ejercicio de imprimir mediante array posiciones
-                    char character = '#';
-                    char space = ' ';
-                    char[] CharToArray = ("").ToCharArray();
-                    int Cantidad = 6;
-
-                    for (int x = 1; x <= Cantidad; x++)
-                    {
-                        string Response = new string(space, Cantidad - x) + new string(character, x);
-                        Console.WriteLine(Response);
-                    }
-                    #endregion
+                    ImplementOrder();
                     break;
                 case 2:
-                    #region eje 
-                    double Cantidad_array = arr.Count();
-                    decimal Resultado = 0;
-                    int[] numerosPositivos = arr.Where(num => num > 0).DefaultIfEmpty(404).ToArray();
-                    int[] numerosNegativos = arr.Where(num => num < 0).DefaultIfEmpty(404).ToArray();
-                    int[] numerosZero = arr.Where(num => num == 0).DefaultIfEmpty(404).ToArray();
-
-                    if (numerosPositivos[0].Equals(404))
-                        Resultado = (decimal)Convert.ToDouble((int)0 / Cantidad_array);
-                    else
-                        Resultado = (decimal)Convert.ToDouble((int)numerosPositivos.Count() / Cantidad_array);
-
-                    Resultado = Math.Round(Resultado, (int)6);
-
-                    Console.WriteLine(Resultado);
-
-                    Resultado = 0;
-                    if (numerosNegativos[0].Equals(404))
-                        Resultado = (decimal)Convert.ToDouble((int)0 / Cantidad_array);
-                    else
-                        Resultado = (decimal)Convert.ToDouble((int)numerosNegativos.Count() / Cantidad_array);
-
-                    Resultado = Math.Round(Resultado, (int)6);
-
-                    Console.WriteLine(Resultado);
-
-                    Resultado = 0;
-                    if (numerosZero[0].Equals(404))
-                        Resultado = (decimal)Convert.ToDouble((int)0 / Cantidad_array);
-                    else
-                        Resultado = (decimal)Convert.ToDouble((int)numerosZero.Count() / Cantidad_array);
-
-                    Resultado = Math.Round(Resultado, (int)6);
-
-                    Console.WriteLine(Resultado);
-                    #endregion
+                    Eje(arr);
                     break;
                 case 3:
                     val1 = Convert.ToInt32(Console.ReadLine());
@@ -100,9 +68,11 @@ namespace ConsoleApp1
                     break;
                 case 4:
                     sum = simpleArraySum(ar);
+                    Console.WriteLine(sum);
                     break;
                 case 5:
                     long result = aVeryBigSum(ar_Long);
+                    Console.WriteLine(result);
                     break;
                 case 6:
                     int n = Convert.ToInt32(Console.ReadLine().Trim());
@@ -115,15 +85,37 @@ namespace ConsoleApp1
                     }
 
                     result_int = diagonalDifference(arr2);
+                    Console.WriteLine(result_int);
                     break;
                 case 7:
-                    int n_new = Convert.ToInt32(Console.ReadLine().Trim());
+                    try
+                    {
+                        Console.WriteLine("Ingrese un número entero:");
+                        int n_new = Convert.ToInt32(Console.ReadLine().Trim());
 
-                    List<int> arr3 = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+                        Console.WriteLine("Ingrese una lista de números separados por espacios:");
+                        List<int> arr3 = Console.ReadLine()
+                                                .Trim()
+                                                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                                .Select(int.Parse)
+                                                .ToList();
+                        plusMinus(arr3);
+                        // Ahora puedes usar n_new y arr3 según sea necesario
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Error: Se esperaba un número entero.");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Error: El número ingresado es demasiado grande o demasiado pequeño para ser representado como un número entero.");
+                    }
 
-                    plusMinus(arr3);
+
+
                     break;
                 case 8:
+                    Console.WriteLine("Ingrese un número entero:");
                     int n_new8 = Convert.ToInt32(Console.ReadLine().Trim());
 
                     staircase(n_new8);
@@ -203,17 +195,50 @@ namespace ConsoleApp1
                     }
                     break;
                 default:
-                    
+
 
 
                     break;
             }
 
-            
-            
-           
+
+
+
 
             Console.ReadLine();
+        }
+        static void ImplementOrder()
+        {
+            #region Ejercicio de imprimir mediante array posiciones
+            char character = '#';
+            char space = ' ';
+            char[] CharToArray = ("").ToCharArray();
+            int Cantidad = 6;
+
+            for (int x = 1; x <= Cantidad; x++)
+            {
+                string Response = new string(space, Cantidad - x) + new string(character, x);
+                Console.WriteLine(Response);
+            }
+            #endregion
+        }
+        static void Eje(int[] arr) {
+
+            #region eje 
+            double Cantidad_array = arr.Count();
+            decimal Resultado = 0;
+            int[] numerosPositivos = arr.Where(num => num > 0).DefaultIfEmpty(404).ToArray();
+            int[] numerosNegativos = arr.Where(num => num < 0).DefaultIfEmpty(404).ToArray();
+            int[] numerosZero = arr.Where(num => num == 0).DefaultIfEmpty(404).ToArray();
+
+            if (numerosPositivos[0].Equals(404))
+                Resultado = (decimal)Convert.ToDouble((int)0 / Cantidad_array);
+            else
+                Resultado = (decimal)Convert.ToDouble((int)numerosPositivos.Count() / Cantidad_array);
+
+            Resultado = Math.Round(Resultado, (int)6);
+
+            Console.WriteLine(Resultado);
         }
         static int solveMeFirst(int a, int b)
         {
@@ -330,6 +355,46 @@ namespace ConsoleApp1
 
 
         }
+
+        public static List<int> GenerateRandomNumbers()
+        {
+            // Crear una lista para almacenar los números aleatorios
+            List<int> randomNumbers = new List<int>();
+
+            // Generar 100 números positivos aleatorios
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int randomNumber = random.Next(1, 1001);
+                randomNumbers.Add(randomNumber);
+            }
+
+            return randomNumbers;
+        }
+
+
     }
-    
+
+    public class RandomService
+    {
+        private readonly Random random;
+
+        public RandomService()
+        {
+            random = new Random();
+        }
+
+        public List<int> GenerateRandomNumbers(int count)
+        {
+            List<int> randomNumbers = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                randomNumbers.Add(random.Next(1, 1001));
+            }
+
+            return randomNumbers;
+        }
+    }
+
 }
